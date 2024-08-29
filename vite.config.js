78eea7e9aaf2a4d10c1f1948459513d715b24a7e
RuanPablo-7 <ToyaMON777@outlook.com>
2024-08-29 15:04:27 -0300

    corrigindo erros

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "chat-frontend",
-  server: {
-    host: true,
-    port: 5173,
-    open: true,
-  }
+  base: "/chat-frontend/",
+
 })
 
